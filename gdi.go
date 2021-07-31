5b0d609a186e088a6133edb0ab1935fe48816f3c
jqzhang <s_jqzhang@163.com>
2021-07-31 15:25:02 +0800

    refactor the lock

--- gdi.go
+++ gdi.go
@@ -59,7 +59,7 @@ func (gdi *GDIPool) RegisterObject(funcObjOrPtrs ...interface{}) {
 		if err != nil {
 			panic(err)
 		}
-		gdi.bind(ftype, funcObjOrPtr)
+		gdi.set(ftype, funcObjOrPtr)
 	}
 }
 
@@ -70,13 +70,23 @@ func (gdi *GDIPool) Build() *GDIPool {
 	return gdi
 }
 
+func (gdi *GDIPool) all() map[reflect.Type]reflect.Value {
+	objs:=make(map[reflect.Type]reflect.Value)
+	gdi.ttvLocker.Lock()
+	defer gdi.ttvLocker.Unlock()
+	for k,v:=range gdi.typeToValues {
+		objs[k]=v
+	}
+	return objs
+}
+
 func (gdi *GDIPool) build(v reflect.Value) {
 	for i := 0; i < v.Elem().NumField(); i++ {
 		if (v.Elem().Field(i).Kind() == reflect.Ptr || v.Elem().Field(i).Kind() == reflect.Interface) && v.Elem().Field(i).IsNil() {
 			ftype := reflect.TypeOf(v.Elem().Field(i).Interface())
 			if ftype == nil { //当为接口时ftype为空
 				isExist := false
-				for t, vTmp := range gdi.typeToValues {
+				for t, vTmp := range gdi.all() {
 					if t.Implements(v.Elem().Field(i).Type()) {
 						v.Elem().Field(i).Set(vTmp)
 						isExist = true
@@ -87,7 +97,7 @@ func (gdi *GDIPool) build(v reflect.Value) {
 					panic(fmt.Sprintf("inject type %v not found,please Register first!!!!", v.Elem().Field(i).Type()))
 				}
 			} else {
-				if value, ok := gdi.typeToValues[ftype]; ok {
+				if value, ok := gdi.get(ftype); ok {
 					v.Elem().Field(i).Set(value)
 				} else {
 					panic(fmt.Sprintf("inject type %v not found,please Register first!!!!", ftype))
@@ -120,10 +130,10 @@ func (gdi *GDIPool) get(t reflect.Type) (result reflect.Value, ok bool) {
 func create(fun interface{}) reflect.Value {
 	values := reflect.ValueOf(fun).Call([]reflect.Value{})
 	if len(values) == 0 {
-		throwPanic(fun)
+		panic(fmt.Sprintf("Dependency injector: func return value must be a pointer or a pointer with error, %v", reflect.TypeOf(fun)))
 	}
 	if len(values) > 2 {
-		throwPanic(fun)
+		panic(fmt.Sprintf("Dependency injector: func return value must be a pointer or a pointer with error, %v", reflect.TypeOf(fun)))
 	}
 	if len(values) == 2 && values[1].Interface() != nil {
 		panic(fmt.Sprintf("init %v throw %v", reflect.TypeOf(fun), reflect.ValueOf(values[1]).Interface()))
@@ -131,12 +141,12 @@ func create(fun interface{}) reflect.Value {
 	return values[0]
 }
 
-func throwPanic(fun interface{}) {
-	errMsg := fmt.Sprintf("Dependency injector: func return value must be a pointer or a pointer with error, %v", reflect.TypeOf(fun))
-	panic(errMsg)
-}
+
 
 func (gdi *GDIPool) set(outType reflect.Type, f interface{}) {
+	if _,ok:=gdi.get(outType);ok {
+		panic(fmt.Sprintf("double register %v", outType))
+	}
 	gdi.creatorLocker.Lock()
 	defer gdi.creatorLocker.Unlock()
 	if reflect.TypeOf(f).Kind() == reflect.Func && f != nil {
@@ -155,15 +165,6 @@ func (gdi *GDIPool) set(outType reflect.Type, f interface{}) {
 	}
 }
 
-func (gdi *GDIPool) bind(outType reflect.Type, f interface{}) {
-
-	if _, ok := gdi.get(outType); ok {
-		panic(fmt.Sprintf("double register %v", outType))
-	} else {
-		gdi.set(outType, f)
-	}
-
-}
 
 func parsePoolFunc(f interface{}) (outType reflect.Type, e error) {
 	ftype := reflect.TypeOf(f)
