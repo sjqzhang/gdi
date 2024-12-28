750fbecdcc87f0a712e98f5df042cd3bc9c38917
sjqzhang <s_jqzhang@163.com>
2024-12-29 03:54:52 +0800

    add method get before after handler

--- annotation.go
+++ annotation.go
@@ -17,7 +17,7 @@ type Context struct {
 }
 
 // AnnotationFunc 注解处理函数类型
-type AnnotationFunc func(ctx *Context) error
+type AnnotationFunc func(ctx *Context)
 
 var (
 	beforeHandlers = make(map[string]AnnotationFunc)
@@ -34,34 +34,44 @@ func RegisterAnnotation(name string, before, after AnnotationFunc) {
 	}
 }
 
+// 获取前置注解处理函数
+func GetBeforeAnnotationHandler(name string) (AnnotationFunc, bool) {
+	return beforeHandlers[name], beforeHandlers[name] != nil
+}
+
+// 获取后置注解处理函数
+func GetAfterAnnotationHandler(name string) (AnnotationFunc, bool) {
+	return afterHandlers[name], afterHandlers[name] != nil
+}
+
 // 内置的日志注解处理函数
 func init() {
 	// 注册日志注解
 	RegisterAnnotation("log",
-		func(ctx *Context) error {
+		func(ctx *Context) {
 			fmt.Printf("[%s] Entering method: %s\n", time.Now().Format("2006-01-02 15:04:05"), ctx.Method)
 			fmt.Printf("Arguments: %v\n", ctx.Args)
-			return nil
+
 		},
-		func(ctx *Context) error {
+		func(ctx *Context) {
 			duration := ctx.EndTime.Sub(ctx.StartTime)
 			fmt.Printf("[%s] Exiting method: %s (duration: %v)\n",
 				time.Now().Format("2006-01-02 15:04:05"),
 				ctx.Method,
 				duration)
 			fmt.Printf("Returns: %v\n", ctx.Returns)
-			return nil
+
 		},
 	)
 
 	// 注册计时器注解
 	RegisterAnnotation("timer", nil,
-		func(ctx *Context) error {
+		func(ctx *Context) {
 			duration := ctx.EndTime.Sub(ctx.StartTime)
 			if duration > 100*time.Millisecond {
 				fmt.Printf("[SLOW] Method %s took %v to execute\n", ctx.Method, duration)
 			}
-			return nil
+
 		},
 	)
 }
